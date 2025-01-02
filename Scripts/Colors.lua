7b49d0d823f2d5d1afe6a6a7b6dd1d206cafe2d3
KyDash <ky_daash@yahoo.com>
2025-01-02 16:27:59 -0400

    Update colours to be less saturated on text elements
    essentially a port of https://github.com/Simply-Love/Simply-Love-SM5/commit/d7192907c9dd96498738a12dbd6abd8d43bf6af5
    ensures that elements that contain text remain legible with a proper contrast

--- Scripts/Colors.lua
+++ Scripts/Colors.lua
@@ -1,7 +1,7 @@
 function PlayerColor( pn )
 	if pn == PLAYER_1 then return DifficultyColor(3) end
 	if pn == PLAYER_2 then return DifficultyColor(1) end
-	return "1,1,1,1"
+	return "HexToRGB(GlobalColors.White)"
 end
 
 function DefaultColor()
@@ -31,90 +31,121 @@ function IconCrop(self)
 	self:cropbottom(1-Color()/12)
 end
 
-function DifficultyColor( dc )
-	if dc == DIFFICULTY_EDIT then return "#B4B7BA" end
-	local color = dc + Color() + 10
-	color = math.mod(color-1,12)+1
-	if color == 1 then return "#FF7D00" end
-	if color == 2 then return "#FF3C23" end
-	if color == 3 then return "#FF003C" end
-	if color == 4 then return "#C1006F" end
-	if color == 5 then return "#8200A1" end
-	if color == 6 then return "#413AD0" end
-	if color == 7 then return "#0073FF" end
-	if color == 8 then return "#00ADC0" end
-	if color == 9 then return "#5CE087" end
-	if color == 10 then return "#AEFA44" end
-	if color == 11 then return "#FFFF00" end
-	if color == 12 then return "#FFBE00" end
-	return "#FFFFFF"
+local function HexToRGB(hexStr)
+	if type(hexStr) ~= 'string' then return 1, 1, 1, 1 end
+	local channels = {} string.gsub(hexStr, '%x%x', function(c) table.insert(channels, tonumber(c, 16)) return channels end)
+	return channels[1] / 255, channels[2] / 255, channels[3] / 255, (channels[4] or 255) / 255
 end
 
-function BubbleColorRGB ( pn )
+-- names taken from https://colordesigner.io/color-name-finder
+-- common colours that do not change depending on initial color selection
+local GlobalColors = {
+	Edit = "#B4B7BA", -- Silver
+	White = "#FFFFFF", -- White
+	Black = "#000000", -- Black
+}
+
+-- colours that the theme will use, either for text on dark backgrounds or background featuring dark text
+-- see https://github.com/Simply-Love/Simply-Love-SM5/commit/d7192907c9dd96498738a12dbd6abd8d43bf6af5
+local Colors = {
+	"#FF7D00", -- Dark Orange
+	"#FF5D47", -- Tomato
+	"#FF577E", -- Indian Red
+	"#FF47B3", -- Deep Pink
+	"#DD57FF", -- Dark Orchid
+	"#8885ff", -- Medium Purple
+	"#3D94FF", -- Dodger Blue
+	"#00B8CC", -- Dark Turquoise
+	"#5CE087", -- Light Green
+	"#AEFA44", -- Green Yellow
+	"#FFFF00", -- Yellow
+	"#FFBE00", -- Gold
+}
+
+-- coloured background elements that do not feature text
+-- original colours used by Simply Love
+local DecorativeColors = {
+	"#FF7D00", -- Dark Orange
+	"#FF3C23", -- Orange Red
+	"#FF003C", -- Crimson
+	"#C1006F", -- Medium Violet Red
+	"#8200A1", -- Dark Orchid
+	"#413AD0", -- Medium Slate Blue
+	"#0073FF", -- Royal Blue
+	"#00ADC0", -- Dark Turquoise
+	"#5CE087", -- Light Green
+	"#AEFA44", -- Green Yellow
+	"#FFFF00", -- Yellow
+	"#FFBE00", -- Gold
+}
+
+function DifficultyColor( dc, decorative )
+	if dc == DIFFICULTY_EDIT then return GlobalColors.Edit end
+	local colorIndex = dc + Color() + 10
+	colorIndex = math.mod(colorIndex-1,12)+1
+	local colorTable = decorative and DecorativeColors or Colors
+	if colorTable[colorIndex] then return colorTable[colorIndex] end
+	return GlobalColors.White
+end
+
+function BubbleColorRGB ( pn, decorative )
 	if GAMESTATE:IsPlayerEnabled( pn ) then
 		local steps = GAMESTATE:GetCurrentSteps( pn )
 		if not steps then return 1,1,1,0 end
 		steps = steps:GetDifficulty()
-		if steps == DIFFICULTY_EDIT	then return 0.71,0.72,0.73,1 end
-		return ColorRGB(steps-2)
+		if steps == DIFFICULTY_EDIT	then return HexToRGB(GlobalColors.Edit) end
+		return ColorRGB(steps-2, decorative)
 	end
-	return 1,1,1,1
+	return HexToRGB(GlobalColors.White)
 end
 
-function DifficultyColorRGB( n ) if n < 5 then return ColorRGB( n - 2 ) else return 0.71,0.72,0.73,1 end end
+function DifficultyColorRGB( n, decorative )
+	if n < 5 then
+		return ColorRGB( n - 2, decorative )
+	else
+		return HexToRGB(GlobalColors.Edit)
+	end
+end
 
-function ColorRGB ( n )
-	local color = n + Color() + 12
-	color = math.mod(color-1,12)+1
-	if color == 1 then return 1,0.49,0,1 end
-	if color == 2 then return 1,0.24,0.14,1 end
-	if color == 3 then return 1,0,0.24,1 end
-	if color == 4 then return 0.76,0,0.44,1 end
-	if color == 5 then return 0.51,0,0.63,1 end
-	if color == 6 then return 0.25,0.23,0.82,1 end
-	if color == 7 then return 0,0.45,1,1 end
-	if color == 8 then return 0,0.68,0.75,1 end
-	if color == 9 then return 0.36,0.88,0.53,1 end
-	if color == 10 then return 0.68,0.98,0.27,1 end
-	if color == 11 then return 1,1,0,1 end
-	if color == 12 then return 1,0.75,0,1 end
-	return 1,1,1,1
+function ColorRGB ( n, decorative )
+	local colorIndex = n + Color() + 12
+	colorIndex = math.mod(colorIndex-1,12)+1
+	local colorTable = decorative and DecorativeColors or Colors
+	if colorTable[colorIndex] then return HexToRGB(colorTable[colorIndex]) end
+	return HexToRGB(GlobalColors.White)
 end
 
 -- Get a color to show text on top of difficulty frames.
 function ContrastingDifficultyColor( dc )
-	if dc == DIFFICULTY_BEGINNER	then return "#000000" end
-	if dc == DIFFICULTY_EASY		then return "#000000" end
-	if dc == DIFFICULTY_MEDIUM		then return "#000000" end
-	if dc == DIFFICULTY_HARD		then return "#000000" end
-	if dc == DIFFICULTY_CHALLENGE	then return "#000000" end
-	if dc == DIFFICULTY_EDIT		then return "#000000" end
-	return "1,1,1,1"
+	if dc == DIFFICULTY_BEGINNER	then return GlobalColors.Black end
+	if dc == DIFFICULTY_EASY		then return GlobalColors.Black end
+	if dc == DIFFICULTY_MEDIUM		then return GlobalColors.Black end
+	if dc == DIFFICULTY_HARD		then return GlobalColors.Black end
+	if dc == DIFFICULTY_CHALLENGE	then return GlobalColors.Black end
+	if dc == DIFFICULTY_EDIT		then return GlobalColors.Black end
+	return GlobalColors.White
 end
 
-function TextOnColor (n)
-	local color = Color() + 12
-	if n then color = color + n end
-	color = math.mod(color-1,12)+1
-	if color == 9 then return 0,0,0,1 end
-	if color == 10 then return 0,0,0,1 end
-	if color == 11 then return 0,0,0,1 end
-	if color == 12 then return 0,0,0,1 end
-	return 1,1,1,1
+function TextOnColor (n, decorative)
+	if not decorative then return HexToRGB(GlobalColors.Black) end
+	local colorIndex = Color() + 12
+	if n then colorIndex = colorIndex + n end
+	colorIndex = math.mod(colorIndex-1,12)+1
+	if colorIndex >= 9 then return HexToRGB(GlobalColors.Black) end
+	return HexToRGB(GlobalColors.White)
 end
 
-function BubbleColorText ( pn )
+function BubbleColorText ( pn, decorative )
+	if not decorative then return HexToRGB(GlobalColors.Black) end
 	if GAMESTATE:IsPlayerEnabled( pn ) then
 		local steps = GAMESTATE:GetCurrentSteps( pn )
-		if not steps then return 1,1,1,0 end
+		if not steps then return HexToRGB('#FFFFFF00') end
 		steps = steps:GetDifficulty()
-		if steps == DIFFICULTY_EDIT	then return 0,0,0,1 end
-		local color = Color() + 10 + steps
-		color = math.mod(color-1,12)+1
-		if color == 9 then return 0.20,0.24,0.26,1 end
-		if color == 10 then return 0.20,0.24,0.26,1 end
-		if color == 11 then return 0.20,0.24,0.26,1 end
-		if color == 12 then return 0.20,0.24,0.26,1 end
+		if steps == DIFFICULTY_EDIT	then return HexToRGB(GlobalColors.Black) end
+		local colorIndex = Color() + 10 + steps
+		colorIndex = math.mod(colorIndex-1,12)+1
+		-- if colorIndex >= 9 then return HexToRGB('#333D42') end
+		if colorIndex >= 9 then return HexToRGB(GlobalColors.Black) end
 	end
-	return 1,1,1,1
+	return HexToRGB(GlobalColors.White)
 end

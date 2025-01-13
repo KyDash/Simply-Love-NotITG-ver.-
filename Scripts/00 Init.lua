ffae501b269cdbfb172b35651643f6e28cf5a9b9
KyDash <ky_daash@yahoo.com>
2025-01-13 01:47:41 -0400

    per column colors, base for panes

--- Scripts/00 Init.lua
+++ Scripts/00 Init.lua
@@ -1,4 +1,5 @@
 -- table for things the theme will use in multiple places, this file *needs* to be executed first
 _SL = {
     Crazy = 1,
+    modulo = function(a, b) return a - math.floor(a / b) * b end
 }
